132f537bad70ede51c9982d9e1f762e31f3d94a0
Getty Ritter <gdritter@galois.com>
2015-06-10 13:05:49 -0700

    Updated point.rs for Rust 1.0 release

--- point.rs
+++ point.rs
@@ -1,27 +1,5 @@
 #![crate_type = "lib"]
 
-// this will silence the warning about the fact that this uses unstable
-// IO features:
-#![feature(io)]
-
-// this silences the warning about the .as_slice() function on line 58,
-// which will eventually be replaced by &foo[..] or something.
-#![feature(core)]
-
-use std::io::Write;
-
-// The println! macro depends on thread-local storage, which Haskell/C
-// have probably not initialized. (Or, rather, the std::io interface
-// does, which means that instead I print using this small helper
-// function instead.) This seemed inconsistently necessary: on some
-// computers, I could use println! without problems, whereas on others,
-// I would get segfaults. This worked consistently:
-fn print_safe(s: &str) {
-    // failing is not really an option if we're calling from Haskell,
-    // so we throw away the result in case it doesn't work.
-    let _ = std::io::stdout().write(s.as_bytes());
-}
-
 // We aren't exposing the internals of this, so I don't bother giving it
 // a C representation.
 pub struct Point {
@@ -63,5 +41,5 @@ pub extern fn add_point(p1: &mut Point, p2: &Point) {
 // Wrap our print method for exporting.
 #[no_mangle]
 pub extern fn print_point(p: &Point) {
-    print_safe(format!("Point(x={}, y={})\n", p.x, p.y).as_slice());
+    println!("Point(x={}, y={})", p.x, p.y);
 }
